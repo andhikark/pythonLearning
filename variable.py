e7cd178de28cd1a050f0e487c6a01961a2af3d3c
Andhika_RK <andhikarestu873@gmail.com>
2024-01-13 20:15:44 +0700

    variable.py

---
+++ variable.py
@@ -0,0 +1,26 @@
+#variable = container for a value
+
+#String
+fname = "Andhika"
+lname = "Restu"
+full_name = fname + " " + lname
+
+print(type(fname)) #define var type
+print("hello" + full_name) #type cast = manually cast type
+
+# #int
+age = 20
+age += 1
+print(type(age))
+print("your age is " + str(age))
+
+# #float
+height = 182.5
+print(type(height))
+print("your high is " + str(height) + "cm")
+
+# #boolean
+human = False
+print(type(human))
+print("are you a human? " + str(human))
+
