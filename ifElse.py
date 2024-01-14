40698c88f9b3888d16e58eff303337998079278a
Andhika_RK <andhikarestu873@gmail.com>
2024-01-14 17:23:47 +0700

    add loop, list, tuple, sets

--- ifElse.py
+++ ifElse.py
@@ -9,3 +9,10 @@ elif age < 0:
     print("Wow how can you have minus age")
 else:
     print("please enter correctly ")
+
+temp = int(input("What is the temperature outside?: "))
+
+if not(temp >= 0 and temp <= 30):
+    print("teh temperature is good today!")
+elif (temp < 0 or temp > 30):
+    print("the temperature is bad today!")
